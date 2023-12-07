230887a7e191165ae2b3587b05121a80c58e326a
Altynai-Dev <altynai.imnl@gmail.com>
2023-12-07 10:40:23 -0400

    first commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: 'What is the purpose of the "setTimeout" function in JavaScript?',
+        answer: 'B. Sets a timer to execute a function after a specified delay',
+        options: [
+            'A. Pauses the execution of the script for a specified duration',
+            'B. Sets a timer to execute a function after a specified delay',
+            'C. Measures the time it takes for a function to execute',
+            'D. Defines the time zone for date and time operations'
+        ]
+    },
+    {
+        numb: 2,
+        question: 'What is the purpose of the "typeof" operator in JavaScript?',
+        answer: 'B. To determine the data type of a variable',
+        options:[
+            'A. To check if a variable is defined',
+            'B. To determine the data type of a variable',
+            'C. To convert a variable to a string',
+            'D. To create a new variable'
+        ]
+    },
+    {
+        numb: 3,
+        question: 'What does the "=== " operator do in JavaScript?',
+        answer: 'A. Checks for equality, including type',
+        options:[
+            'A. Checks for equality, including type',
+            'B. Checks for equality, excluding type',
+            'C. Assigns a value to a variable',
+            'D. Concatenates two strings'
+        ]
+    },
+    {
+        numb: 4,
+        question: 'What is the role of the "event.preventDefault()" method in JavaScript?',
+        answer: 'B. Prevents the default behavior of an event',
+        options:[
+            'A. Stops the propagation of an event',
+            'B. Prevents the default behavior of an event',
+            'C. Executes the default behavior of an event',
+            'D. Removes an event listener'
+        ]
+    },
+    {
+        numb: 5,
+        question: 'What is the purpose of the "map" function in JavaScript?',
+        answer: 'A. To create a new array by applying a function to each element of an existing array',
+        options:[
+            'A. To create a new array by applying a function to each element of an existing array',
+            'B. To filter elements in an array based on a given condition',
+            'C. To sort the elements of an array in ascending order',
+            'D. To remove the last element from an array'
+        ]
+    }
+]
